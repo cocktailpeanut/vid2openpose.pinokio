9671df830de599b43809f10dffcd30e1f250ae4f
cocktailpeanut <cocktailpeanuts@proton.me>
2023-12-17 17:39:53 -0500

    update

--- pinokio.js
+++ pinokio.js
@@ -4,13 +4,25 @@ module.exports = {
   description: "Turn any video into Openpose video https://huggingface.co/spaces/fffiloni/video2openpose2",
   menu: async (kernel) => {
     let installed = await kernel.exists(__dirname, "app", "env")
-    if (installed) {
+    let installing = kernel.running(__dirname, "install.json")
+    if (installing) {
+      return [{ icon: "fa-solid fa-plug", text: "Installing", href: "install.json", params: { fullscreen: true, run: true } }]
+    } else if (installed) {
       let running = await kernel.running(__dirname, "start.json")
       if (running) {
-        return [
-          { icon: "fa-solid fa-spin fa-circle-notch", text: "Running" },
-          { icon: "fa-solid fa-terminal", text: "Terminal", href: "start.json" }
-        ]
+        let session = await kernel.require(__dirname, "session.json")
+        if (session && session.url) {
+          return [
+            { icon: "fa-solid fa-spin fa-circle-notch", text: "Running" },
+            { icon: "fa-solid fa-rocket", text: "Web UI", href: session.url, target: "_blank" },
+            { icon: "fa-solid fa-terminal", text: "Terminal", href: "start.json" }
+          ]
+        } else {
+          return [
+            { icon: "fa-solid fa-spin fa-circle-notch", text: "Running" },
+            { icon: "fa-solid fa-terminal", text: "Terminal", href: "start.json" }
+          ]
+        }
       } else {
         return [
           { icon: "fa-solid fa-power-off", text: "start", href: "start.json", params: { fullscreen: true, run: true } },
